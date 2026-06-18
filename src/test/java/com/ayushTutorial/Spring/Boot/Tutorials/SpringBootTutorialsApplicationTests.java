b68bda6541cefc7e0010037114d32229d476cb20
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2026-06-18 10:33:53 +0530

    Spring Boot Tutorials

---
+++ src/test/java/com/ayushTutorial/Spring/Boot/Tutorials/SpringBootTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushTutorial.Spring.Boot.Tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
